8b8fce6079e92eedcc2b965d9e1c04648abbfb9e
Marcin Mosiolek <marcin.mosiolek@tooploox.com>
2018-04-15 14:29:49 +0200

    M

--- train.py
+++ train.py
@@ -9,6 +9,58 @@ from tools import make_var
 from progress.bar import Bar
 
 
+def validate(model, criterion, valid_x, valid_y, batch_size=128):
+    losses = []
+    data_len = len(valid_x)
+
+    progress = Bar("Validation", max=data_len)
+
+    for i in range(0, data_len, batch_size):
+        # process batches
+        x = valid_x[i: i + batch_size]
+        y = valid_y[i: i + batch_size]
+        # make cuda variables
+        x = make_var(x)
+        y = make_var(y)
+        # make predictions
+        predicted_y = model(x)
+        loss = criterion(predicted_y, y)
+        losses.append(loss)
+        # monitor progress
+        progress.next()
+    progress.finish()
+
+    return np.mean(losses)
+
+
+def train_step(model, criterion, optimizer, train_x, train_y, batch_size=128):
+    losses = []
+    data_len = len(train_x)
+
+    progress = Bar("Training", max_data_len)
+
+    for i in range(0, data_len, batch_size):
+        # process batches
+        x = train_x[i: i + batch_size]
+        y = train_y[i: i + batch_size]
+        # make cuda variables
+        x = make_var(x)
+        y = make_var(y)
+        # make predictions
+        predicted_y = model(x)
+        loss = criterion(predicted_y, y)
+        # backprop
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.append(loss)
+        # monitor progress
+        progress.next()
+    progress.finish()
+
+    return np.mean(losses)
+
+
 def main(num_epochs = 100, batch_size = 128, learning_rate = 1e-3):
     # load data
     data = DataLoader("../autencoder/convex_hulls.npy", batch_size=batch_size)
@@ -21,30 +73,12 @@ def main(num_epochs = 100, batch_size = 128, learning_rate = 1e-3):
 
     # train the stuff
     for epoch in range(num_epochs):
-        progress = Bar("Training", max=data.len())
-        for x, y in data:
-            progress.next()
-            #print(x.shape)
-            x = make_var(x)
-            y = make_var(y)
-            # ===================forward=====================
-            predicted_y = model(x)
-            loss = criterion(predicted_y, y)
-            # ===================backward====================
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        # ===================log========================
-        valid_x, valid_y = data.valid_data()
-        valid_x = make_var(valid_x)
-        valid_y = make_var(valid_y)
-        predicted_y = model(valid_x)
-        valid_loss = criterion(predicted_y, valid_y)
+        train_loss = train_step(model, criterion, optimizer, data.train_x, data.train_y)
+        valid_loss = validate(model, criterion, data.valid_x, data.valid_y)
 
         print('epoch [{}/{}], train loss:{:.4f}, valid loss:{:.4f}'
-              .format(epoch + 1, num_epochs, loss.data[0], valid_loss.data[0]))
+              .format(epoch + 1, num_epochs, train_loss, valid_loss))
 
-        progress.finish()
     torch.save(model.state_dict(), './conv_autoencoder.pth')
 
 if __name__ == "__main__":
