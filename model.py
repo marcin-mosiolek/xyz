12ccc636f397f1d17d5f0a74ad4e2541631bde52
Marcin Mosiolek <marcin.mosiolek@tooploox.com>
2018-04-15 19:05:17 +0200

    M

--- model.py
+++ model.py
@@ -5,20 +5,20 @@ class AutoEncoder(nn.Module):
         super(AutoEncoder, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 8, 8, stride=1, padding=0),  # b, 16, 10, 10
+            nn.Conv2d(1, 8, 8, stride=1, padding=0),
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),  # b, 16, 5, 5
-            nn.Conv2d(8, 16, 6, stride=1, padding=0),  # b, 8, 3, 3
+            nn.MaxPool2d(2, stride=1),
+            nn.Conv2d(8, 16, 6, stride=1, padding=0),
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+            nn.MaxPool2d(2, stride=1)
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(16, 8, 6, stride=1),  # b, 16, 5, 5
+            nn.ConvTranspose2d(16, 8, 6, stride=1),
             nn.ReLU(True),
-            nn.ConvTranspose2d(8, 8, 8, stride=1, padding=0),  # b, 8, 15, 15
+            nn.ConvTranspose2d(8, 8, 8, stride=1, padding=0),
             nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, 3, stride=1, padding=0),  # b, 1, 28, 28
+            nn.ConvTranspose2d(8, 1, 3, stride=1, padding=0),
             nn.Tanh()
         )
 
