500e9dd48d65f5fd5f5f89f77a28a9cb58b9b669
Marcin Mosiolek <marcin.mosiolek@tooploox.com>
2018-04-17 13:01:10 +0200

    asd

--- tools.py
+++ tools.py
@@ -14,16 +14,16 @@ class DataLoader(object):
         self.y = y
 
         self.pivot = int(len(x) * 0.75)
-        self.train_x = self.x[:self.pivot]
-        self.train_y = self.y[:self.pivot]
-        self.valid_x = self.x[self.pivot:]
-        self.valid_y = self.y[self.pivot:]
+        self.train_x = x[:self.pivot]
+        self.train_y = y[:self.pivot]
+        self.valid_x = x[self.pivot:]
+        self.valid_y = y[self.pivot:]
 
         self.train_x = self.normalize(self.train_x)
         self.train_y = self.normalize(self.train_y)
         self.valid_x = self.normalize(self.valid_x)
         self.valid_y = self.normalize(self.valid_y)
-        
+
     def normalize(self, data):
         data[data > 0] = 1.0
         return data
